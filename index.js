2ec3a00de4bbcaa46ce52df8e0e20c6d3b956227
Dev Jeff <arribion@gmail.com>
2025-11-16 18:38:23 +0300

    first commit project setup

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import ejs from "ejs";
+import axios from "axios";
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+dotenv.config();
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+app.use("public", express.static(__dirname));
+
+app.get("/", (req, res) => {
+    res.send("<h1>implement mpesa stk push</h1>"); 
+});
+
+app.get('/send', (req, res) => {
+    
+})
+
+// axios.post()
+
+app.listen(process.env.PORT, () => {
+    console.log(`http://localhost:${process.env.PORT}`)
+})
