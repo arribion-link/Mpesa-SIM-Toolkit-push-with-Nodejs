5669be3b75b120c212b6b916913fa79ea4696bc8
Dev Jeff <arribion@gmail.com>
2025-11-23 18:57:03 +0300

    added content to the controller

--- controllers/lipa.Controller.js
+++ controllers/lipa.Controller.js
@@ -1,5 +1,31 @@
+import axios from "axios";
+import timestamp from "";
+
+const mpesa_safaricom_url = process.env.MPESA_SANDBOX_URL;
+
+const payload = {
+    BusinessShortCode: BUSINESS_SHORT_CODE,
+    Password: password,
+    Timestamp: timestamp,
+    TransactionType: "CustomerPayBillOnline",
+    Amount: amount,
+    PartyA: phone,
+    PartyB: process.env.MPESA_BUSINESS_SHORT_CODE,
+    PhoneNumber: phone,
+    CallBackURL: "https://buysasaOnline.com/",
+    AccountReference: "BuySasa online shop",
+    TransactionDesc: "Payment",
+};
+
 const lipaController = async (req, res) => {
-    
+    const { phoneNumber, amount } = req.body;
+    if (!phoneNumber || !amount) {
+        res.json({
+            succcess: false,
+            msg: "Please provide all the fields"
+        });
+    }
+    axios.post(mpesa_safaricom_url, payload);
 }
 
 export default lipaController;
